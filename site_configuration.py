1e9b989f407922df78de3dc678c6e8ced9abc115
Alexander Marquardt <alexander.marquardt@gmail.com>
2013-06-04 12:11:49 -0400

    Fixed the registration fields - prefviously had a table inside a table which was very ugly.

--- site_configuration.py
+++ site_configuration.py
@@ -29,7 +29,7 @@ import os, datetime, logging
 
 from rs.private_data import *
 
-VERSION_ID = '2013-06-02-2048'
+VERSION_ID = '2013-06-03-1828'
 
 # The following must be set to True before uploading - can be set to False for debugging js/css as modifications are made
 USE_TIME_STAMPED_STATIC_FILES = False
@@ -65,12 +65,12 @@ if os.path.isdir('rs/proprietary'):
         # the build name.
         
         BUILD_NAME = 'Discrete'     # originally used for RomanceSecreto.com
-        #BUILD_NAME = 'Single'   # originally used for SingletonSearch.com
-        #BUILD_NAME = 'Language'  # originally used for LikeLanguage.com
-        #BUILD_NAME = 'Swinger'   # originally used for SwingerSetup.com
-        #BUILD_NAME = 'Lesbian'   # originally used for LesbianHeart.com
-        #BUILD_NAME = 'Gay'       # originally used for GaySetup.com
-        #BUILD_NAME = 'Friend'    # originally used for FriendBazaar.com
+        BUILD_NAME = 'Single'   # originally used for SingletonSearch.com
+        BUILD_NAME = 'Language'  # originally used for LikeLanguage.com
+        BUILD_NAME = 'Swinger'   # originally used for SwingerSetup.com
+        BUILD_NAME = 'Lesbian'   # originally used for LesbianHeart.com
+        BUILD_NAME = 'Gay'       # originally used for GaySetup.com
+        BUILD_NAME = 'Friend'    # originally used for FriendBazaar.com
         BUILD_NAME = 'Mature' # originallly used for MellowDating.com
     
     else:
