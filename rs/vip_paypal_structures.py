c9b77f64dd9a964da69c8b2c9b437c7a17b8d35c
Alexander Marquardt <alexander.marquardt@gmail.com>
2013-09-03 22:49:50 -0400

    Added 3-month payment option, and reduced prices.

--- rs/vip_paypal_structures.py
+++ rs/vip_paypal_structures.py
@@ -33,12 +33,12 @@ from localization_files import currency_by_country
 #VIP_1_DAY = "1_day" # for testing only
 VIP_3_DAYS  = "3 days"
 VIP_1_MONTH = "1 month"
-#VIP_3_MONTHS = "3 months"
+VIP_3_MONTHS = "3 months"
 VIP_6_MONTHS = "6 months"
 VIP_1_YEAR  = "1 year"
 
 # the following list will allow us to iterate over the various membership options in the correct order
-vip_membership_categories = [VIP_3_DAYS, VIP_1_MONTH, VIP_6_MONTHS, VIP_1_YEAR]
+vip_membership_categories = [VIP_3_DAYS, VIP_1_MONTH, VIP_3_MONTHS, VIP_6_MONTHS, VIP_1_YEAR]
 SELECTED_VIP_DROPDOWN = VIP_6_MONTHS
 
 # Leave the following value set to None if we are not trying to force a particular country's options to be displayed
@@ -48,7 +48,7 @@ num_days_in_vip_membership_category = {
     #VIP_1_DAY : 1,
     VIP_3_DAYS  : 3,
     VIP_1_MONTH : 31,
-    #VIP_3_MONTHS : 92,
+    VIP_3_MONTHS : 92,
     VIP_6_MONTHS : 183,
     VIP_1_YEAR  : 365,    
 }
@@ -58,7 +58,7 @@ vip_option_values = {
     #VIP_1_DAY : {'duration' : "1", 'duration_units' : "day"},
     VIP_3_DAYS : {'duration': "3", 'duration_units' : ugettext_lazy("days")}, 
     VIP_1_MONTH: {'duration': "1", 'duration_units' : ugettext_lazy("month")},
-    #VIP_3_MONTHS: {'duration': "3", 'duration_units' : ugettext_lazy("months")},
+    VIP_3_MONTHS: {'duration': "3", 'duration_units' : ugettext_lazy("months")},
     VIP_6_MONTHS: {'duration': "6", 'duration_units' : ugettext_lazy("months")},
     VIP_1_YEAR: {'duration': "1", 'duration_units' : ugettext_lazy("year")},
 }
@@ -68,24 +68,24 @@ vip_paypal_prices = {
     'EUR': {
         VIP_3_DAYS: "6.95",
         VIP_1_MONTH: "19.95",
-        #VIP_3_MONTHS: "37.95",
-        VIP_6_MONTHS: "49.95",
-        VIP_1_YEAR: "99.95",
+        VIP_3_MONTHS: "29.95",
+        VIP_6_MONTHS: "39.95",
+        VIP_1_YEAR: "69.95",
         },
     'USD' : {
         VIP_3_DAYS: "6.95",
         VIP_1_MONTH: "19.95",
-        #VIP_3_MONTHS: "37.95",
-        VIP_6_MONTHS: "49.95",
-        VIP_1_YEAR: "99.95",
+        VIP_3_MONTHS: "29.95",
+        VIP_6_MONTHS: "39.95",
+        VIP_1_YEAR: "69.95",
         }, 
     'MXN' : {
         # 1 USD = 13 MXN
         VIP_3_DAYS: "69.95",    # 
         VIP_1_MONTH: "199.95",  # 
-        #VIP_3_MONTHS: "379.95", # 
-        VIP_6_MONTHS: "499.95", # 
-        VIP_1_YEAR: "999.95",   
+        VIP_3_MONTHS: "299.95", # 
+        VIP_6_MONTHS: "399.95", # 
+        VIP_1_YEAR: "699.95",   
         },    
     #'GBP' : {
         #VIP_1_WEEK: ".95",
