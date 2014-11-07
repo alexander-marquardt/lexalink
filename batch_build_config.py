c6f1e6d31ee4f8f95a8dc4baf32010e1314b46cf
Alexander Marquardt <alexander.marquardt@gmail.com>
2014-11-07 08:53:51 -0500

    Switched build order

--- batch_build_config.py
+++ batch_build_config.py
@@ -1,3 +1,3 @@
-BUILD_NAMES_LIST = ['discrete_build', 'single_build' , 'language_build', 'lesbian_build', 'mature_build', 'gay_build', 'swinger_build', 'friend_build']
+BUILD_NAMES_LIST = ['discrete_build', 'single_build' ,  'lesbian_build', 'language_build', 'mature_build', 'gay_build', 'swinger_build', 'friend_build']
 TARGET_DIR = "/Users/alexandermarquardt/Lexabit/lexalink-uploads"
 BUILD_NAMES_LIST = ['discrete_build']
