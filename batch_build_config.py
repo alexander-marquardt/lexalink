1e0603398304ac9d8b04348e261e36e997369f5f
Alexander Marquardt <alexander.marquardt@gmail.com>
2014-12-31 18:34:03 -0500

    Updated to work with new Linux-based uploading system instead of old Windows-based system.

--- batch_build_config.py
+++ batch_build_config.py
@@ -1,3 +1,4 @@
 BUILD_NAMES_LIST = ['discrete_build', 'single_build' ,  'lesbian_build', 'language_build', 'mature_build', 'gay_build', 'swinger_build', 'friend_build']
 TARGET_DIR = "/Users/alexandermarquardt/Lexabit/lexalink-uploads"
+TARGET_DIR_FOR_UPLOAD = "/media/psf/Lexabit/lexalink-uploads"
 BUILD_NAMES_LIST = ['discrete_build']
