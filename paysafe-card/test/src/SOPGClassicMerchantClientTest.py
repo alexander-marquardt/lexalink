cf283f94dca454e09888ecd3444f70735c98fe6c
Alexander Marquardt <alexander.marquardt@gmail.com>
2015-01-27 15:01:34 -0500

    removed 'params' dictionary and access variables directly.

--- paysafe-card/test/src/SOPGClassicMerchantClientTest.py
+++ paysafe-card/test/src/SOPGClassicMerchantClientTest.py
@@ -35,10 +35,12 @@ wsdl_url = 'http://localhost:8000/paysafecard/sopg_wsdl.xml'
 username = 'Lexabit_Inc_test'
 password = 'uOLSLoFmktH7DyB'
 currency = 'EUR'
+amount = 0.01
 mid = '1000005878'
 ok_url = urllib.quote('http://www.%s/psc/okurl/' % site_configuration.DOMAIN_NAME, '')
 nok_url = urllib.quote('http://www.%s/psc/nokurl/' % site_configuration.DOMAIN_NAME, '')
 pn_url = urllib.quote('http://www.%s/paysafe/ipn/' % site_configuration.DOMAIN_NAME, '')
+merchant_client_id = 'foobartesting-id-1'
 client_ip = None # This will be set once we get farther along into the paysafe integration
 
 card_pin = '8691159531990439'
@@ -52,16 +54,8 @@ class TestSOPGClassicMerchantClient(unittest.TestCase):
 
     def setUp(self):
         self.mtid = 'temporary-testing-id-' + str(time.time())
-        self.client = SOPGClassicMerchantClient(wsdl_url, self.params['endpoint'])
+        self.client = SOPGClassicMerchantClient(wsdl_url, endpoint)
 
-    # def test_01_SOPGClassicMerchantClient(self):
-    #     try:
-    #         pass
-    #     except Exception as e:
-    #         print ('Error creating SOAP client: ', e)
-    #         self.assertTrue(False)
-    #     else:
-    #         self.assertTrue(True)
 
     def test_02_CreateDisposition(self):
         response = self.createDisposition()
@@ -69,155 +63,23 @@ class TestSOPGClassicMerchantClient(unittest.TestCase):
         self.assertEqual(response['errorCode'], 0)
         self.assertEqual(response['resultCode'], 0)
 
-    # def test_03_AssignCardToDisposition(self):
-    #     self.createDisposition()
-    #     response = self.assignCardToDisposition()
-    #
-    #     self.assertEqual(response['errorCode'], 0)
-    #     self.assertEqual(response['resultCode'], 0)
-    #
-    # def test_04_AssignCardsToDisposition(self):
-    #     self.createDisposition()
-    #
-    #     response = self.assignCardsToDisposition()
-    #
-    #     self.assertEqual(response['errorCode'], 0)
-    #     self.assertEqual(response['resultCode'], 0)
-    #
-    # def test_05_ModifyDispositionValue(self):
-    #     self.createDisposition()
-    #     self.assignCardToDisposition()
-    #     response = self.client.modifyDispositionValue(
-    #         self.params['username'],
-    #         self.params['password'],
-    #         self.mtid,
-    #         None,
-    #         self.params['amount'],
-    #         self.params['currency'],
-    #         )
-    #     self.assertEqual(response['errorCode'], 0)
-    #     self.assertEqual(response['resultCode'], 0)
-    #
-    # def test_06_GetSerialNumbers(self):
-    #     self.createDisposition()
-    #     response = self.client.getSerialNumbers(
-    #         self.params['username'],
-    #         self.params['password'],
-    #         self.mtid,
-    #         None,
-    #         self.params['currency'],
-    #         )
-    #     self.assertEqual(response['errorCode'], 0)
-    #     self.assertEqual(response['resultCode'], 0)
-    #
-    # def test_07_GetDispositionRawState(self):
-    #     self.createDisposition()
-    #     response = self.client.getDispositionRawState(
-    #         self.params['username'],
-    #         self.params['password'],
-    #         self.mtid,
-    #         None,
-    #         self.params['currency'],
-    #         )
-    #     self.assertEqual(response['errorCode'], 0)
-    #     self.assertEqual(response['resultCode'], 0)
-    #
-    # def test_08_ExecuteDebit(self):
-    #     self.createDisposition()
-    #     self.assignCardToDisposition()
-    #     response = self.client.executeDebit(
-    #         self.params['username'],
-    #         self.params['password'],
-    #         self.mtid,
-    #         None,
-    #         self.params['amount'],
-    #         self.params['currency'],
-    #         1,
-    #         None
-    #     )
-    #     self.assertEqual(response['errorCode'], 0)
-    #     self.assertEqual(response['resultCode'], 0)
-    #
-    # def test_09_GetMid(self):
-    #     response = self.client.getMid(
-    #         self.params['username'],
-    #         self.params['password'],
-    #         self.params['currency'],
-    #         )
-    #     self.assertEqual(response['errorCode'], 0)
-    #     self.assertEqual(response['resultCode'], 0)
 
     def createDisposition(self):
         createDisposition = self.client.createDisposition
         return createDisposition(
-            self.params['username'],
-            self.params['password'],
+            username,
+            password,
             self.mtid,
             None,
-            self.params['amount'],
-            self.params['currency'],
-            self.params['okUrl'],
-            self.params['nokUrl'],
-            None,None,None
+            amount,
+            currency,
+            ok_url,
+            nok_url,
+            merchant_client_id,
+            pn_url,
+            client_ip,
         )
 
-    def assignCardToDisposition(self):
-        assignCardToDisposition = self.client.assignCardToDisposition
-        return assignCardToDisposition(
-            self.params['username'],
-            self.params['password'],
-            self.mtid,
-            None,
-            self.params['amount'],
-            self.params['currency'],
-            self.params['pin'],
-            )
-
-    def assignCardsToDisposition(self):
-        return self.client.assignCardsToDisposition(
-            self.params['username'],
-            self.params['password'],
-            self.mtid,
-            None,
-            self.params['amount'],
-            self.params['currency'],
-            None,
-            1,
-            self.cards)
-
-    # params = {
-    #               'endpoint' : 'https://soatest.paysafecard.com/psc/services/PscService?wsdl',
-    #               'username' : 'USER_CLASSIC',
-    #               'password' : 'PASS',
-    #               'currency' : 'EUR',
-    #               'amount' : 0.01,
-    #               'okUrl' : 'http://okurl',
-    #               'nokUrl' : 'http://nokurl',
-    #               'pin' : '5000000000002517',
-    #               'mid' : '1000001243',
-    #               'serialNumber' : '5000000000002517;0.01',
-    #               'dispositionState' : 'S',
-    #       }
-
-    params = {
-        'endpoint' : endpoint,
-        'username' : username,
-        'password' : password,
-        'currency' : currency,
-        'amount' : 0.01,
-        'okUrl' : ok_url,
-        'nokUrl' : nok_url,
-        'pin' : card_pin,
-        'mid' : mid,
-        'serialNumber' : '%s;0.01' % card_serial_number,
-        'dispositionState' : 'S',
-        }
-
-    card1 = {}
-    card1['pin']= card_pin
-    card2 = {}
-    card2['pin']='5000000000002511'
-    cards = [card1, card2]
 
     if __name__ == '__main__':
         unittest.main()
