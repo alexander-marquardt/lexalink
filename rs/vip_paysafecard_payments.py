46fc54de5d3aba80f7a5abbe1736524e2b6e5c99
Alexander Marquardt <alexander.marquardt@gmail.com>
2015-01-28 19:48:04 -0500

    Added initial version of paysafecard payment radio buttons

--- rs/vip_paysafecard_payments.py
+++ rs/vip_paysafecard_payments.py
@@ -3,7 +3,16 @@ import logging
 from django.shortcuts import render_to_response
 from django import http
 
+import site_configuration
+
 from rs import error_reporting
+from rs import vip_payments_common
+
+vip_paysafecard_valid_currencies = ['EUR', 'USD', 'MXN', 'USD_NON_US']
+
+
+vip_paysafecard_prices_with_currency_units = vip_payments_common.generate_prices_with_currency_units(
+    vip_payments_common.vip_standard_membership_prices, vip_paysafecard_valid_currencies)
 
 def paysafecard_sopg_wsdl(request):
     try:
@@ -13,3 +22,64 @@ def paysafecard_sopg_wsdl(request):
     except:
         error_reporting.log_exception(logging.critical)
         return http.HttpResponseServerError('Error accessing paysafecard_wsdl.xml document')
+    
+
+def generate_paysafe_dropdown_options(currency):
+    # for efficiency don't call this from outside this module, instead perform a lookup in
+    # paypal_dropdown_options
+    generated_html = u''
+    for member_category in vip_payments_common.vip_membership_categories:
+        duration = u"%s" % vip_payments_common.vip_option_values[member_category]['duration']
+        duration_units = u"%s" % vip_payments_common.vip_option_values[member_category]['duration_units']
+
+        if member_category == vip_payments_common.SELECTED_VIP_DROPDOWN:
+            selected = "checked"
+        else:
+            selected = ''
+
+        generated_html += u"""<input type="radio" name="amount" value="%(total_price)s" %(selected)s>
+        <strong>%(duration)s %(duration_units)s</strong>: %(total_price)s<br>\n""" % {
+            'duration': duration, 'duration_units' : duration_units,
+            'selected' : selected,
+            'total_price' : vip_paysafecard_prices_with_currency_units[currency][member_category]}
+
+    return generated_html
+
+def generate_paysafecard_data(request, owner_nid):
+
+    try:
+        # Get the ISO 3155-1 alpha-2 (2 Letter) country code, which we then use for a lookup of the
+        # appropriate currency to display. If country code is missing, then we will display
+        # prices for the value defined in vip_paypal_structures.DEFAULT_CURRENCY
+        if not vip_payments_common.TESTING_COUNTRY:
+            http_country_code = request.META.get('HTTP_X_APPENGINE_COUNTRY', None)
+        else:
+            error_reporting.log_exception(logging.error, error_message = "TESTING_COUNTRY is over-riding HTTP_X_APPENGINE_COUNTRY")
+            http_country_code = vip_payments_common.TESTING_COUNTRY
+
+        internal_currency_code = vip_payments_common.get_internal_currency_code(http_country_code, vip_paysafecard_valid_currencies)
+
+        paysafecard_data = {}
+        paysafecard_data['owner_nid'] = owner_nid
+        paysafecard_data['currency_code'] = vip_payments_common.real_currency_codes[internal_currency_code]
+        paysafecard_data['country_override'] = vip_payments_common.TESTING_COUNTRY
+        paysafecard_data['testing_paysafecard'] = site_configuration.TESTING_PAYSAFECARD
+
+
+        # if not site_configuration.TESTING_PAYSAFECARD:
+        #     paysafecard_data['paysafecard_soap_username'] = site_configuration.PAYSAFE_SOAP_TEST_USERNAME
+        #     paysafecard_data['paysafecard_soap_password'] = site_configuration.PAYSAFE_SOAP_TEST_PASSWORD
+        #     paysafecard_data['paysafecard_merchant_id'] = site_configuration.PAYSAFE_TEST_MID
+        #
+        # else:
+        #     paysafecard_data['paysafecard_soap_username'] = site_configuration.PAYSAFE_SOAP_USERNAME
+        #     paysafecard_data['paysafecard_soap_password'] = site_configuration.PAYSAFE_SOAP_PASSWORD
+        #     paysafecard_data['paysafecard_merchant_id'] = site_configuration.PAYSAFE_MID
+
+
+        paysafecard_data['dropdown_options'] = generate_paysafe_dropdown_options(internal_currency_code)
+
+        return paysafecard_data
+    except:
+        error_reporting.log_exception(logging.critical)
+        return http.HttpResponseServerError('Error generating paysafecard data')
--- rs/vip_render_purchase_options.py
+++ rs/vip_render_purchase_options.py
@@ -5,6 +5,7 @@ from django.template import loader, Context
 from rs import constants
 from rs import error_reporting
 from rs import vip_paypal_payments
+from rs import vip_paysafecard_payments
 
 def render_purchase_buttons(request, username, owner_nid):
 
@@ -15,11 +16,12 @@ def render_purchase_buttons(request, username, owner_nid):
                 # only show payment options/buttons to users that are logged-in.
 
                 paypal_data = vip_paypal_payments.generate_paypal_data(request, username, owner_nid)
+                paysafecard_data = vip_paysafecard_payments.generate_paysafecard_data(request, owner_nid)
                 #fortumo_data = generate_fortumo_data(request, username, owner_nid)
                 template = loader.get_template("user_main_helpers/purchase_buttons.html")
                 context = Context (dict({
                     'paypal_data': paypal_data,
-                    #'fortumo_data' : fortumo_data,
+                    'paysafecard_data' : paysafecard_data,
                     'request' : request,
                     }, **constants.template_common_fields))
                 return template.render(context)
